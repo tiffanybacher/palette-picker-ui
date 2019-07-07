b4ca895353900e0cdc4fbd1b5c398e63f965d0d8
tiffanybacher <tiffanybacher@gmail.com>
2019-07-07 15:43:35 -0600

    Create initial ProjectCard component

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectCard(props) {
+  return (
+    <article className="ProjectCard">
+      <p>Project Card</p>
+    </article>
+  );
+}
+
+export default ProjectCard;
