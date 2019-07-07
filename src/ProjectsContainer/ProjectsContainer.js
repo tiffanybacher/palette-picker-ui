5f08f6adeb176eaf1b03e16b76a84b6b6672f9ee
tiffanybacher <tiffanybacher@gmail.com>
2019-07-07 15:07:41 -0600

    Set up ProjectsContainer components and snapshot test

---
+++ src/ProjectsContainer/ProjectsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProjectsContainer extends Component {
+  
+
+  render() {
+    return(
+      <section className="ProjectsContainer">
+        <h2>Your Projects</h2>
+      </section>
+    );
+  }
+}
+
+export default ProjectsContainer;
---
+++ src/ProjectsContainer/ProjectsContainer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProjectsContainer from './ProjectsContainer';
+
+describe('ProjectsContainer', () => {
+  let wrapper;
+
+  beforeEach(()=> {
+    wrapper = shallow(
+      <ProjectsContainer />
+    );
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
