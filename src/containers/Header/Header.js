c991d870efc6e41f6a7dbb2b34ab926a14b7a9dc
Brennan <brennanduffey@gmail.com>
2019-07-07 12:37:38 -0600

    Add mapstatetoprops in header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function Header() {
+function Header(props) {
   return (
     <h1>Palette Picker</h1>
     {!props.isLoggedIn && <p>LOGIN</p>}
     {props.isLoggedIn && <p>SIGN OUT</p>}
   );
 };
+
+export const mapStateToProps = (state) => ({
+  isLoggedIn: state.isLoggedIn
+})
