5ba92ff6a84b0f9f4e71ed136e60f93d39857b1c
tiffanybacher <tiffanybacher@gmail.com>
2019-07-07 11:03:02 -0600

    Add Open Sans to font family

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Palette Picker</h1>
       </header>
     </div>
