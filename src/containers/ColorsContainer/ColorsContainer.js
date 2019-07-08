d4b534696f702dd5c7adb9ef012268cd7a467b9d
Brennan <brennanduffey@gmail.com>
2019-07-07 19:40:41 -0600

    Change ColorsContainer to class component and generate colors

--- src/containers/ColorsContainer/ColorsContainer.js
+++ src/containers/ColorsContainer/ColorsContainer.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-function ColorsContainer(props) {
-  const colors = props.colors.map(color => (
-    <Color ...color setColors={props.setColors} />
+class ColorsContainer extends Component {
+
+  componentDidMount() {
+    this.generateColors();
+  };
+
+  randomColorGenerator = () => {
+    let randomColor = "#000000".replace(/0/g, function() {
+      return (~~(Math.random() * 16)).toString(16);
+    });
+    return randomColor;
+  }; 
+
+  generateColors = () => {
+    let colors = [];
+    let color;
+    for (let i = 0; i < 5; i++) {
+      color = { isLocked: false, hexCode: this.randomColorGenerator() };
+      colors.push(color)
+    };
+    this.props.setColors();
+  };
+
+  colors = this.props.colors.map(color => (
+    <Color ...color setColors={this.props.setColor} />
   ));
 
-  return (
-    {colors}
-  );
+  render() {(
+    {this.colors}
+  )};
 };
 
 export const mapStateToProps = (state) => ({
@@ -16,7 +38,8 @@ export const mapStateToProps = (state) => ({
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  setColor: (color) => dispatch(setColor(color)) 
+  setColor: (color) => dispatch(setColor(color)),
+  setColors: (colors) => dispatch(setColors(colors))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ColorsContainer);
