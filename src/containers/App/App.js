8c7649e26afd70d3c767c6e61b9a07479e76dc70
tiffanybacher <tiffanybacher@gmail.com>
2019-07-08 17:57:42 -0600

    Set up Routes in App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import ColorsContainer from '../containers/ColorsContainer/ColorsContainer.js';
-import Header from '../containers/Header/Header.js';
+import ColorsContainer from '../ColorsContainer/ColorsContainer';
+import Header from '../Header/Header';
 import ProjectsContainer from '../ProjectsContainer/ProjectsContainer';
-import CreateAccount from '../CreateAccount/CreateAccount'
+import CreateAccount from '../CreateAccount/CreateAccount';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ColorsContainer />
-      <header>
-        <h1>Palette Picker</h1>
-      </header>
-      <ProjectsContainer />
-      <CreateAccount />
+      <Route path="/home" component={ColorsContainer} />
+      {
+        //<Route path="/home/login" component={Login} />
+      }
+      {
+        //<Route path="/home/save" component={} />
+      }
+      <Route path="/projects" component={ProjectsContainer} />
+      <Route path="/home/register" component={CreateAccount} />
     </div>
   );
 }
