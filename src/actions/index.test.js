22adb71fdb0b4cfa9cde62caa8ff1e7ae5b904e5
Brennan <brennanduffey@gmail.com>
2019-07-09 15:48:32 -0600

    Add actions test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,15 +1,39 @@
 import * as actions from './index';
 
 describe('actions', () => {
-  it('should have a type of SET_COLORS', () => {
-    const expectedAction = actions.setColors([])
-    expect(expectedAction.type).toEqual('SET_COLORS');
+  describe('setColors action creator', () => {
+    it('should have a type of SET_COLORS', () => {
+      const expectedAction = actions.setColors([])
+      expect(expectedAction.type).toEqual('SET_COLORS');
+    });
+
+    it('should have a property of colors', () => {
+      const expectedAction = actions.setColors([]);
+      expect(expectedAction.colors).toEqual([]);
+    });
   });
 
-  it('should have a type of SET_COLORS', () => {
-    const expectedAction = actions.setColors([]);
-    expect(expectedAction.colors).toEqual([]);
+  describe('toggleLocked action creator', () => {
+    it('should have a type of TOGGLE_LOCKED', () => {
+      const expectedAction = actions.toggleLocked(7);
+      expect(expectedAction.type).toEqual('TOGGLE_LOCKED');
+    });
+
+    it('should have a property of id', () => {
+      const expectedAction = actions.toggleLocked(7);
+      expect(expectedAction.id).toEqual(7);
+    });
   });
 
-  it('should have a type of ')
+  describe('setUser action creator', () => {
+    it('should have a type of SET_USER', () => {
+      const expectedAction = actions.setUser({});
+      expect(expectedAction.type).toEqual('SET_USER');
+    });
+
+    it('should have a property of user that matches argument', () => {
+      const expectedAction = actions.setUser({});
+      expect(expectedAction.user).toEqual({});
+    });
+  });
 });
