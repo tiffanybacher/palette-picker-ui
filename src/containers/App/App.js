de08475407be37b6ecbfce47982b090c9a0222ab
Brennan <brennanduffey@gmail.com>
2019-07-08 20:25:26 -0600

    Refactor Header to use NavLinks

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,20 +4,20 @@ import Header from '../Header/Header';
 import ProjectsContainer from '../ProjectsContainer/ProjectsContainer';
 import CreateAccount from '../CreateAccount/CreateAccount';
 import { Route } from 'react-router-dom';
+import Login from '../Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Route path="/home" component={ColorsContainer} />
-      {
-        //<Route path="/home/login" component={Login} />
-      }
+      <Route path="/home/login" component={Login} />
       {
         //<Route path="/home/save" component={} />
       }
       <Route path="/projects" component={ProjectsContainer} />
       <Route path="/home/register" component={CreateAccount} />
+      <Route path="/signout" />
     </div>
   );
 }
