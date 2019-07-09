30b9fb01f5fa85597bd9d31348bc1051e404951a
Brennan <brennanduffey@gmail.com>
2019-07-08 21:05:24 -0600

    Add login HTML and connect to redux

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 
 function Login() {
-
+  <form>
+    <h3>Login</h3>
+    <label for="username">USERNAME:</label>
+    <input name="username" />
+    <label for="password">PASSWORD:</label>
+    <input name="password"/> 
+  </form>
 };
 
-export default Login;
+export const mapDispatchToProps = (dispatch) => ({
+  setUser: (user) => dispatch(setUser(user))
+});
+
+
+
+export default connect(null, dispatch)(Login);
