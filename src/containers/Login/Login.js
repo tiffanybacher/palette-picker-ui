ffddbc87d291517fa35fe1e3eac20d48af0fb08d
Brennan <brennanduffey@gmail.com>
2019-07-09 21:49:36 -0600

    Add redirect from login form

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setUser } from '../../actions';
-import { Link } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 class Login extends Component {
   constructor() {
@@ -16,24 +16,27 @@ class Login extends Component {
     this.setState({ [e.target.name]:e.target.value })
   };
 
-  postUser = async (e) => {
+  getUser = async (e) => {
     e.preventDefault();
-    const request = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      }, 
-      body: JSON.stringify(this.state)
+    const { username, password } = this.state;
+    try {
+      const response = await fetch(`http://localhost:3001/api/v1/users/${username}/${password}`);
+      if(!response.ok) {
+        throw Error('error in fetching user')
+      }
+      const result = await response.json();
+      this.props.setUser(result)
+    } catch (error) {
+       console.log(error);
     };
-    const response = await(fetch('/api/v1/users', request));
-    // if response is not okay add to local state and display on form
-    // if successful save user to store and redirect to home
-    // fetch projects and palettes
   };
 
   render() {
+    if(this.props.user.username) {
+      return <Redirect to='/home' />
+    }
     return (
-      <form onSubmit={this.postUser} className="login-form">
+      <form onSubmit={this.getUser} className="login-form">
         <h3>Login</h3>
         <label htmlFor="username">USERNAME:</label>
         <input onChange={this.handleChange} name="username" />
@@ -41,12 +44,16 @@ class Login extends Component {
         <input onChange={this.handleChange} name="password"/> 
         <button type="submit"> Login </button>
         <p>
-          Not a member? <Link to="/home/register">Create an account?</Link>
+          Not a member? <Link to="/register">Create an account?</Link>
         </p>
       </form>
     );
   };
 };
+
+export const mapStateToProps = (state) => ({
+  user: state.user
+});
 
 export const mapDispatchToProps = (dispatch) => ({
   setUser: (user) => dispatch(setUser(user))
@@ -54,4 +61,4 @@ export const mapDispatchToProps = (dispatch) => ({
 
 
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
