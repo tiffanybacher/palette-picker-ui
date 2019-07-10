b01e33ba5ed22e6236d00b0a4f739ae1bb104fc4
Brennan <brennanduffey@gmail.com>
2019-07-10 16:18:03 -0600

    Add conditional rendering to savepaletteform

--- src/components/SavePalette/SavePalette.js
+++ src/components/SavePalette/SavePalette.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SavePalette extends Component {
   constructor() {
     super();
     this.state = {
       name: '',
-      isNewProject: false
+      isNewProject: true
     };
   };
 
@@ -13,25 +14,36 @@ class SavePalette extends Component {
     this.setState({ [e.target.name]:e.target.value });
   };
 
-  addNewProject = (e) => {
+  createProject = (e) => {
     this.setState({ isNewProject: true });
   };
 
-  // add to existing project conditionally rendered on projects in store
-  //  
+  addToProject = (e) => {
+    this.setState({ isNewProject: false })
+  }
 
   render() {
+    const options = this.props.projects.map(project => {
+      return <option value={project.name}> {project.name} </option>
+    });
+
     return (
       <form>
         <label htmlFor="name"> PALETTE NAME:</label>
         <input name="name" onChange={this.handleChange}/>
-        <p>Add to existing project</p>
-        {this.state.isNewProject && <input />}
-        <p>Create new project</p>
+        <p onClick={this.addToProject}>Add to existing project</p>
+        <p onClick={this.createProject}>Create new project</p>
+        {this.state.isNewProject && <input placeholder="Project Name"/>}
+        {!this.state.isNewProject && <select> {options} </select>}
         <button type="submit">SAVE</button>
       </form>
     );
   };
 };
 
-export default SavePalette;
+export const mapStateToProps = (state) => ({
+  user: state.user,
+  projects: state.projects
+});
+
+export default connect(mapStateToProps)(SavePalette);
