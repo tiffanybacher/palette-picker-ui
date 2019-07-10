f01d436a21ca1f936e5467723b7e48055ed206d2
tiffanybacher <tiffanybacher@gmail.com>
2019-07-10 17:45:10 -0600

    Update data structure for a single color

--- src/components/PalettePreview/PalettePreview.js
+++ src/components/PalettePreview/PalettePreview.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 function PalettePreview(props) {
   const defaultColors = [
-  '#FDC58D', 
-  '#FFFFAA', 
-  '#95E38A', 
-  '#779DF2', 
-  '#D2BEFD'
+    { id: 1, hex: '#FDC58D', isLocked: false },
+    { id: 2, hex: '#FFFFAA', isLocked: false },
+    { id: 3, hex: '#95E38A', isLocked: false },
+    { id: 4, hex: '#779DF2', isLocked: false },
+    { id: 5, hex: '#D2BEFD', isLocked: false }
   ];
   const colors = props.colors_array || defaultColors;
   const colorPreviews = colors.map(color => 
     <div 
       className="preview"
-      style={{ background: color}}
-      key={Date.now()}>
+      style={{ background: color.hex}}
+      key={color.id}>
     </div>
   );
 
