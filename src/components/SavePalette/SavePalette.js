16f38712b768e9c377d5b134e70374326c4d3fb7
tiffanybacher <tiffanybacher@gmail.com>
2019-07-11 06:29:49 -0600

    Style SavePalette

--- src/components/SavePalette/SavePalette.js
+++ src/components/SavePalette/SavePalette.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addProject, addPalette } from '../../actions';
-
+import { Link } from 'react-router-dom';
 
 class SavePalette extends Component {
   constructor() {
@@ -82,26 +82,28 @@ class SavePalette extends Component {
 
   render() {
     const options = this.props.projects.map(project => {
-      return <option value={project.id}> {project.name} </option>
+      return <option value={project.id}>{project.name}</option>
     });
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="name"> PALETTE NAME:</label>
-        <input name="paletteName" onChange={this.handleChange}/>
-        <p onClick={this.addToProject}>Add to existing project</p>
-        <p onClick={this.createProject}>Create new project</p>
-        {
-          this.state.isNewProject 
-          && 
-          <input name="projectName" placeholder="Project Name" onChange={this.handleChange}/>
-        }
-        {
-          !this.state.isNewProject 
-          && 
-          <select onChange={this.selectChange}> <option>Please select a project</option> {options} </select>}
-        <button type="submit">SAVE</button>
-      </form>
+      <div className="SavePalette">
+        <form onSubmit={this.handleSubmit} className="save-form">
+          <label htmlFor="name">PALETTE NAME:</label>
+          <input type="text" name="paletteName" onChange={this.handleChange} placeholder="Palette Name"/>
+          <p onClick={this.addToProject}>Add to existing project</p>
+          <p onClick={this.createProject}>Create new project</p>
+          {
+            this.state.isNewProject 
+            && 
+            <input type="text" name="projectName" placeholder="Project Name" onChange={this.handleChange}/>
+          }
+          {
+            !this.state.isNewProject 
+            && 
+            <select onChange={this.selectChange}><option>Please select a project</option>{options}</select>}
+          <button type="submit" className="save-btn"><Link to="/projects" className="save-link">SAVE</Link></button>
+        </form>
+      </div>
     );
   };
 };
