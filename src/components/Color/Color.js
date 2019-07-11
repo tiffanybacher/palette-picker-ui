38b698add08348b6c472cb2b38168dba86cfef62
Brennan <brennanduffey@gmail.com>
2019-07-11 04:10:49 -0600

    Test Color component

--- src/components/Color/Color.js
+++ src/components/Color/Color.js
@@ -4,7 +4,7 @@ function Color(props) {
   return (
     <section className="palette">
       <div className="color" style={{ background: props.hexCode }}></div>
-      {props.isLocked && <p onClick={() => props.toggleLocked(props.id)}> LOCKED </p>}
+      {props.isLocked && <p id="locked" onClick={() => props.toggleLocked(props.id)}> LOCKED </p>}
       {!props.isLocked && <p onClick={() => props.toggleLocked(props.id)}> UNLOCKED </p>}
       <p> {props.hexCode} </p>
     </section>
